7ec466eec50b90018b4bf960216fd2f61de93960
you21979 <you2197901@gmail.com>
2014-09-04 01:09:33 +0900

    mod

--- tutorial31-40/36.go
+++ tutorial31-40/36.go
@@ -2,7 +2,21 @@ package main
 
 import "code.google.com/p/go-tour/pic"
 
+func f(x,y int) uint8 {
+    return uint8(x^y)
+    //return uint8(x*y)
+    //return uint8((x+y)/2)
+}
+
 func Pic(dx, dy int) [][]uint8 {
+    screen := make([][]uint8, dx)
+    for x := 0; x < dx; x++ {
+    	screen[x] = make([]uint8, dy)
+        for y := 0; y < dy; y++ {
+            screen[x][y] = f(x, y)
+        }
+    }
+    return screen
 }
 
 func main() {
