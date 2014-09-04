5621675bc39a59196993fc76780dee9699b4f23e
you21979 <you2197901@gmail.com>
2014-09-04 14:29:49 +0900

    mod

--- tutorial01-10/01.go
+++ tutorial01-10/01.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "net"
-    "os"
-    "time"
-)
+import "fmt"
 
 func main() {
-    fmt.Println("Welcome to the playground!")
-
-    fmt.Println("The time is", time.Now())
-
-    fmt.Println("And if you try to open a file:")
-    fmt.Println(os.Open("filename"))
-
-    fmt.Println("Or access the network:")
-    fmt.Println(net.Dial("tcp", "google.com"))
+    fmt.Println("Hello, 世界")
 }
